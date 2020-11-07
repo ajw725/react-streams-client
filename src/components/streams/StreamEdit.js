05e98bfe41d03a83df4becc776063eb05c448804
Andrew Weinstein <andrew@altvia.com>
2020-11-06 19:16:00 -0700

    filter initial values

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream, updateStream } from '../../actions';
@@ -22,7 +23,7 @@ class StreamEdit extends React.Component {
       <div>
         <h3>Edit Stream: {this.props.stream.title}</h3>
         <StreamForm
-          initialValues={this.props.stream}
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
           onSubmit={this.onSubmit}
         />
       </div>
