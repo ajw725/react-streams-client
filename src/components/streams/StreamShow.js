f9eda5fe7b48dd5b0bd45e78cf306a366a6da604
Andrew Weinstein <andrew@altvia.com>
2020-11-07 15:10:22 -0700

    set up FLV player on stream show

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     const streamId = this.props.match.params.id;
     this.props.fetchStream(streamId);
+    this.initializePlayer();
+  }
+
+  componentDidUpdate() {
+    this.initializePlayer();
+  }
+
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.destroy();
+    }
+  }
+
+  initializePlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${this.props.stream.id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+    // this.player.play();
   }
 
   render() {
@@ -17,6 +49,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
